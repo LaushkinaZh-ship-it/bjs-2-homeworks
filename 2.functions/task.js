523c6dcc2d476eb241d1a38ae681f468d60d49f3
feroiza <LaushkinaZh@yandex.ru>
2026-04-02 19:31:14 +0300

    Домашнее задание к лекции 2 «Функции» полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,72 @@
 function getArrayParams(...arr) {
+    if (arr.length === 0) {
+        return { min: 0, max: 0, avg: 0 };
+    }
 
-  return { min: min, max: max, avg: avg };
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+        sum += arr[i];
+    }
+
+    const avg = Number((sum / arr.length).toFixed(2));
+    return { min, max, avg };
 }
 
 function summElementsWorker(...arr) {
-
+    if (arr.length === 0) return 0;
+    return arr.reduce((sum, num) => sum + num, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+    if (arr.length === 0) return 0;
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+    if (arr.length === 0) return 0;
 
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+
+    for (const num of arr) {
+        if (num % 2 === 0) sumEvenElement += num;
+        else sumOddElement += num;
+    }
+
+    return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+    if (arr.length === 0) return 0;
 
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+
+    for (const num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num;
+            countEvenElement++;
+        }
+    }
+
+    if (countEvenElement === 0) return 0;
+    return Number((sumEvenElement / countEvenElement).toFixed(2));
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
 
+    for (const subArray of arrOfArr) {
+        const result = func(...subArray);
+        if (result > maxWorkerResult) maxWorkerResult = result;
+    }
+
+    return maxWorkerResult;
 }
